41b44be7bfb81bbe65ae5b04f2eb20407bda3212
clair5 <112450618+clair5@users.noreply.github.com>
2022-12-29 21:10:12 +0900

    1229

---
+++ app/src/main/java/com/example/testphp/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.testphp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn12;
+    private TextView tv_id, tv_pass;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv_id = findViewById(R.id.tv_id);
+        tv_pass = findViewById(R.id.tv_pass);
+        btn12 = findViewById(R.id.button12);
+
+        Intent intent = getIntent();
+        String userID = intent.getStringExtra("userID");
+        String userPass = intent.getStringExtra("userPass");
+
+        tv_id.setText(userID);
+        tv_pass.setText(userPass);
+    }
+}
